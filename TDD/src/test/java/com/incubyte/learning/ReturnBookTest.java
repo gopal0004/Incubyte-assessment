ada640eaa1fd45a517ba1a997aceb11470aa1e92
gopal0004 <gopalbera6335@gmail.com>
2024-08-11 03:53:04 +0530

    first case get passed

--- TDD/src/test/java/com/incubyte/learning/ReturnBookTest.java
+++ TDD/src/test/java/com/incubyte/learning/ReturnBookTest.java
@@ -4,7 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class ReturnBookTest {
 
 	LibrarySystem library;

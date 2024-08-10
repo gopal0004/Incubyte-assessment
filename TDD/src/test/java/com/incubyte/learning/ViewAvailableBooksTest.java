f62428cdb9fd5e211f78e3af8857dfae28d70f76
gopal0004 <gopalbera6335@gmail.com>
2024-08-11 04:50:49 +0530

    created viewAvailableBook class and case got failed cause of not implemented yet

---
+++ TDD/src/test/java/com/incubyte/learning/ViewAvailableBooksTest.java
@@ -0,0 +1,14 @@
+package com.incubyte.learning;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ViewAvailableBooksTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
